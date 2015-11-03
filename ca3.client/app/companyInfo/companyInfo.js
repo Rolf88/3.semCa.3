8051b814471d0e64d3474b670ace9d0a5a45c8a0
Alexander Steen <AlexanderSteen@Alexanders-MacBook-Pro.local>
2015-11-03 11:54:20 +0100

    Added company info, now able to search in virk open data and get company info

--- ca3.client/app/companyInfo/companyInfo.js
+++ ca3.client/app/companyInfo/companyInfo.js
@@ -1,21 +1,46 @@
 'use strict';
 
-angular.module('myApp.companyInfo', ['ngRoute'])
+angular.module('myApp.companyInfo', ['ngRoute', 'ui.bootstrap'])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/companyInfo', {
+                    templateUrl: 'companyInfo/companyInfoView.html',
+                    controller: 'companyInfoCtrl'
+                });
+            }])
+        .controller('companyInfoCtrl', ['$http', '$scope', 'companyInfoFactory', function ($http, $scope, companyInfoFactory) {
+                $scope.company = {};
+                $scope.companyFound = false;
+                $scope.searchText = "";
+                $scope.errorMessage = "";
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/companyInfo', {
-    templateUrl: 'companyInfo/companyInfoView.html',
-    controller: 'companyInfoCtrl'
-  });
-}])
+                $scope.getCompany = function () {
+                    $scope.companyFound = false;
+                    companyInfoFactory.searchCompany($scope.searchText).then(function (data) {
+                        var data = data.data;
+                        if (data.length <= 0) {
+                            $scope.errorMessage = "Could not find any results";
+                        } else {
+                            $scope.errorMessage = "";
+                            $scope.companyFound = true;
+                            $scope.company = data;
+                        }
+                    }, function (error) {
+                        if ($scope.searchText.length <= 0) {
+                            $scope.errorMessage = "Please provide a search input";
+                        } else {
+                            $scope.errorMessage = "An error has orcured - See console";
+                            console.log(error);
+                        }
+                    });
+                };
+            }])
+        .factory('companyInfoFactory', ['$http', function ($http) {
+                var urlBase = 'api/company';
+                var companyInfoFactory = {};
 
-.controller('companyInfoCtrl', function($http,$scope) {
-  $http.get('api/demoadmin')
-            .success(function (data, status, headers, config) {
-              $scope.data = data;
-            })
-            .error(function (data, status, headers, config) {
-              
-             });
- 
-});
+                companyInfoFactory.searchCompany = function (search) {
+                    return $http.get(urlBase + '/' + search);
+                };
+
+                return companyInfoFactory;
+            }]);
