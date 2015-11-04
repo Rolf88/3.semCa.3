5e0bb8485eccb48bdf1659e76648940ff1c35b5c
TryingToImprove <privat.oliver.lassen@gmail.com>
2015-11-04 09:34:06 +0100

    Add dailyrates to UI

--- ca3.client/app/exchangeRateInfo/exchangeRateInfo.js
+++ ca3.client/app/exchangeRateInfo/exchangeRateInfo.js
@@ -1,9 +1,27 @@
 'use strict';
 
-angular.module('myApp.exchangeRateInfo', ['ngRoute'])
+var CurrencyModule = angular.module('myApp.exchangeRateInfo', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/exchangeRateInfo', {
-    templateUrl: 'exchangeRateInfo/exchangeRateInfoView.html'
-  });
-}]);
+CurrencyModule.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/exchangeRateInfo', {
+            templateUrl: 'exchangeRateInfo/exchangeRateInfoView.html'
+        });
+    }]);
+
+CurrencyModule.factory("CurrencyFactory", ["$http", function ($http) {
+        return {
+            getDailyRates: function () {
+                return $http.get("api/currency/dailyrates");
+            }
+        };
+    }]);
+
+
+CurrencyModule.controller("DailyRatesController", ["CurrencyFactory", function (CurrencyFactory) {
+        var self = this;
+        self.rate = [];
+        
+        CurrencyFactory.getDailyRates().then(function (response) {
+            self.rate = response.data;
+        });
+    }]);
--- ca3.server/src/java/facades/CurrencyFacade.java
+++ ca3.server/src/java/facades/CurrencyFacade.java
@@ -9,38 +9,40 @@ import javax.persistence.Query;
 import javax.persistence.TemporalType;
 
 public class CurrencyFacade {
-
+    
     private static final HashMap<String, List<CurrencyRate>> rateCache = new HashMap<>();
-
+    
     private final EntityManager entityManager;
-
+    
     public CurrencyFacade(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
-
+    
     public void create(List<CurrencyRate> currencies) {
         this.entityManager.getTransaction().begin();
-
+        
         for (CurrencyRate currency : currencies) {
             this.entityManager.persist(currency);
         }
-
+        
         this.entityManager.getTransaction().commit();
     }
-
+    
     public List<CurrencyRate> find(Date date) {
-        String cacheKey = date.getDate() + "-" + date.getMonth() + "-" + date.getYear();
-
+        date.setDate(date.getDate() - 1);
+        
+        String cacheKey = date.getDate() + "-" + date.getMonth() + "-" + date.getYear();        
+        
         if (!rateCache.containsKey(cacheKey)) {
             Query query = entityManager.createQuery("SELECT c FROM CurrencyRate c WHERE c.timeCreated = :timeCreated");
             query.setParameter("timeCreated", date, TemporalType.DATE);
-
+            
             List<CurrencyRate> result = query.getResultList();
             rateCache.put(cacheKey, result);
-
+            
             return result;
         }
-
+        
         return rateCache.get(cacheKey);
     }
 }
