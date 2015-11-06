d2a573e2413c7ca6e106a3d4a6aa104a7c21a00a
DeLukzCoder <LukaszKrawczyk@Lukaszs-MacBook-Pro.local>
2015-11-06 10:13:00 +0100

    Delete User Function added

--- ca3.client/app/allUsers/allUsers.js
+++ ca3.client/app/allUsers/allUsers.js
@@ -11,6 +11,12 @@ var app = angular.module('myApp.allUsers', ['ngRoute'])
 
         .controller('allUsersCtrl', function ($http, userFactory) {
             var self = this;
+            self.removeUser = function (user) {
+                userFactory.removeUser(user.id).then(function (response) {
+                    var indexOfUser = self.users.indexOf(user);
+                    self.users.splice(indexOfUser, 1);
+                })
+            }
 
             userFactory.getAllUsers().then(function (response) {
                 self.users = response.data;
@@ -22,6 +28,9 @@ app.factory("userFactory", ["$http", function ($http) {
         return{
             getAllUsers: function () {
                 return $http.get("api/admin/users");
+            }
+            , removeUser: function (id) {
+                return $http.delete("api/admin/user/" + id);
             }
         }
     }]);
